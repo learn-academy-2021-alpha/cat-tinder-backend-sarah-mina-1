574610b81f2adc25ecb81d824983f81e51376e73
sjproctor <sjproctor950@gmail.com>
2021-04-01 10:27:00 -0700

    adds seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cats = [
+  {
+    name: 'Moe',
+    age: 7,
+    enjoys: 'begging for food'
+  },
+  {
+    name: 'Biscuits',
+    age: 4,
+    enjoys: 'sitting on the window sill'
+  },
+  {
+    name: 'Gravy',
+    age: 4,
+    enjoys: 'playing with shoestrings'
+  },
+  {
+    name: 'Bruce Willis',
+    age: 7,
+    enjoys: 'being the mayor of the neighborhood'
+  }
+]
+
+cats.each do |cat|
+  Cat.create cat
+  puts "creating the cat: #{cat}"
+end
