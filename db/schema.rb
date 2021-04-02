34f9284ad7f676c0d4f9584a053259387d60fe04
sjproctor <sjproctor950@gmail.com>
2021-04-02 16:20:23 -0700

    adds cors gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_163022) do
+ActiveRecord::Schema.define(version: 2021_04_01_184735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2021_04_01_163022) do
     t.text "enjoys"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "supers", force: :cascade do |t|
+    t.string "name"
+    t.integer "power_level"
+    t.string "place_of_birth"
+    t.text "hobbies"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
 end
